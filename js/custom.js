7bf5a5e6f2bdb042bb8890a30186a429b0fdae0c
Ramanovich <medikroma@gmail.com>
2018-09-04 20:00:07 +0000

    color v1

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,40 @@
 (function($) {
 	'use-strict';
 
-	/* ---------------------------------------------
-	 Initializing Functions Triggered On Window Load
-	 --------------------------------------------- */
+	/* --- Initializing Functions Triggered On Window Load --- */
+
 	$(window).on('load', function() {
 		initPreloaderFade();
 	});
 
 
-	/* ---------------------------------------------
-	 Initializing Functions Triggered On Window Resize
-	 --------------------------------------------- */
+	/* --- Initializing Functions Triggered On Window Resize --- */
+
 	$(window).on('resize', function() {
 		initHeroHeight();
 	});
 
+	/* --- Initializing Functions Triggered On Window Scroll --- */
+	$(window).on('scroll', function() {
+		initHeaderAnimation();
+		initSearchAreaHide();
+	});
 
-	/* ---------------------------------------------
-	 Initializing All Functions
-	 --------------------------------------------- */
+
+	/* --- Initializing All Functions --- */
+
 	initColorScheme();
 	initHeroHeight();
 	initParallax();
 	initSectionHighlight();
 	initPreloaderFade();
+	initAnimateScroll();
+	initNavMenu();
 
 
 
-	/* ---------------------------------------------
-	 Color Scheme Select
-	 --------------------------------------------- */
+	/* --- Color Scheme Select --- */
+
 	function initColorScheme() {
 		$('.color-scheme-select-btn').on('click', function() {
 			$('.color-scheme-select').toggleClass('color-scheme-select-visible');
@@ -43,17 +47,15 @@
 	}
 
 
-	/* ---------------------------------------------
-	 Preloader Fadeout
-	 --------------------------------------------- */
+	/* --- Preloader Fadeout --- */
+
 	function initPreloaderFade() {
 		$('.preloader').fadeOut();
 	}
 
 
-	/* ---------------------------------------------
-	 Section Highlighting
-	 --------------------------------------------- */
+	/* --- Section Highlighting --- */
+
 	function initSectionHighlight() {
 		$('section').each(function() {
 			$(this).waypoint(function(direction) {
@@ -76,23 +78,63 @@
 	}
 
 
-	/* ---------------------------------------------
-	 Hero Height
-	 --------------------------------------------- */
+	/* --- Hero Height --- */
+
 	function initHeroHeight() {
 		$('.hero-height').height($(window).height());
 	}
 
 
-	/* ---------------------------------------------
-	 Parallax Background
-	 --------------------------------------------- */
+	/* --- Parallax Background --- */
+
 	function initParallax() {
 		if (!device.tablet() && !device.mobile()) {
 			$('.parallax-section').each(function() {
 				$('.parallax-section').parallaxScroll("50%", 0.3);
 			});
 			$('.parallax-layer').parallax();
+		}
+	}
+
+
+	/* --- Animate Scroll --- */
+
+	function initAnimateScroll() {
+		$('.animatescroll-link').on('click', function(e) {
+			e.preventDefault();
+		});
+	}
+
+
+	/* --- Navigation Menu --- */
+
+	function initNavMenu() {
+		$('.menu-bar').on('click', function() {
+			if ($('.main-nav').hasClass('hidden')) {
+				$('.main-nav').removeClass('hidden');
+				$('.main-nav').addClass('active');
+			}
+		});
+
+		$('.menu-close').on('click', function() {
+			if ($('.main-nav').hasClass('active')) {
+				$('.main-nav').removeClass('active');
+				$('.main-nav').addClass('hidden');
+			}
+		});
+	}
+
+	/* --- Header Animation --- */
+
+	function initHeaderAnimation() {
+		var scroll_top = $(document).scrollTop();
+		if (scroll_top >= 100) {
+			$('.header').removeClass('header-hidden');
+			$('.header').addClass('header-visible');
+		}
+		else {
+			$('.header').removeClass('header-visible');
+			$('.header').addClass('header-hidden');
 		}
 	}
 
