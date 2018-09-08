d24bc20122c9f69555b7536196b69dbac39db291
Ramanovich <medikroma@gmail.com>
2018-09-08 23:35:34 +0000

    reference

---
+++ js/owl.carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items: 1,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1]
+
+    });
+
+});
+
+console.log ("text");
